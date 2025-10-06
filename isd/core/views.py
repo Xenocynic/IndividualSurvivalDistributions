c16d590c99b4d3cc1d049c08260c9ece5a263426
adviislam10 <sahiislam4@gmail.com>
2025-10-05 18:53:58 -0600

    feat(auth): add JWT authentication with user registration and login
    - Implemented user registration, login, and token refresh using Django REST Framework SimpleJWT.
    - Added email validation and updated URL routing.
    -Tested endpoints via API client.

--- isd/core/views.py
+++ isd/core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
 
-# Create your views here.
+# Register new users
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
