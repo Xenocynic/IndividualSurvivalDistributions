a595cbb8d7707af19398d33574609c7d8e8a7233
Nguyen Huu Huy Hoang <nguyenh226@mymacewan.ca>
2025-10-05 21:29:28 -0600

    Set up database and migration

--- isd/isd/settings.py
+++ isd/isd/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',  # Example API support
     'core'  # main app
 ]
 
@@ -73,10 +74,18 @@ WSGI_APPLICATION = 'isd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# Configure default database to use Supabase PostgreSQL.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Superdatabase1?',
+        'HOST': 'db.ckbvvhnocuavnzqgqitu.supabase.co',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
