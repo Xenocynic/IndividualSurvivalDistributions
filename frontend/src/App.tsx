122f5fdb7b3d52489fb5b8702ddcea412f13d31c
SYED SHAHMEER RAHMAN <adreet21@gmail.com>
2025-10-12 02:14:47 -0600

    US 1.7 Implemented the landing page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import type { JSX } from "react/jsx-runtime";
 import { Routes, Route, Navigate } from 'react-router-dom'
 import AppLayout from './layouts/AppLayout'
 import Dashboard from './pages/Dashboard'
+import Landing from "./pages/Landing";
 
 
 export default function App(): JSX.Element {
@@ -9,10 +10,10 @@ export default function App(): JSX.Element {
     <Routes>
       <Route element={<AppLayout />}>
 
-        <Route index element={<Dashboard />} />
+        <Route index element={<Landing />} />
         <Route path="dashboard" element={<Dashboard />} />
         
-        {/* Add future pages here as soon as they get made - currently: about, instruction, predictors */}
+        {/* Add future pages here as soon as they get made - currently: About, Dashboard */}
         <Route path="*" element={<Navigate to="/" replace />} />
 
       </Route>
