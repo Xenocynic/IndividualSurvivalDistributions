c5f937ee97cc43e5bbdfa4271ea34f0809562daf
Selena Chainani <130656588+Xenocynic@users.noreply.github.com>
2025-10-12 13:36:21 -0600

    hotfix
    made sure to accomodate both new pages for resolving the merge conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,20 +3,23 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import AppLayout from './layouts/AppLayout'
 import Dashboard from './pages/Dashboard'
 import Landing from "./pages/Landing";
+import About from "./pages/About"; 
 
 
 export default function App(): JSX.Element {
   return (
     <Routes>
       <Route element={<AppLayout />}>
-
-        <Route index element={<Landing />} />
-        <Route path="dashboard" element={<Dashboard />} />
         
         {/* Add future pages here as soon as they get made - currently: About, Dashboard */}
         <Route path="*" element={<Navigate to="/" replace />} />
+        <Route index element={<Landing />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="about" element={<About />} />
 
+        {/* keep wildcard last */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
-    )
+  );
 }
