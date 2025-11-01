85d7f72701d9036eadd321ac72613216e78a37e1
adviislam10 <sahiislam4@gmail.com>
2025-11-01 12:29:36 -0600

    Remove unused 'roles' field from accounts

--- isd/accounts/serializers.py
+++ isd/accounts/serializers.py
@@ -9,40 +9,25 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ["id", "username", "email", "first_name", "last_name", "password", "groups", "role", "date_joined", "is_active"]
+        fields = ["id", "username", "email", "first_name", "last_name", "password", "groups", "date_joined", "is_active"]
         extra_kwargs = {
             'password': {'write_only': True},
-            'id': {'read_only': True}
+            'id': {'read_only': True},
+            'date_joined': {'read_only': True},
+            'is_active': {'read_only': True},
         }
     
     def create(self, validated_data):
-        role = validated_data.pop('role', 'user')
+        # Create user with hashed password
         user = User.objects.create_user(**validated_data)
-        
-        # Assign user to role group
-        if role:
-            group, created = Group.objects.get_or_create(name=role)
-            user.groups.add(group)
-        
         return user
     
-    def update(self, instance, validated_data):
-        role = validated_data.pop('role', None)
-        
-        # Update user fields
+    def update(self, instance, validated_data):    
+        # Handle password update separately
+        password = validated_data.pop('password', None)
         for attr, value in validated_data.items():
-            if attr != 'password':
-                setattr(instance, attr, value)
-            else:
-                instance.set_password(value)
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
         instance.save()
-        
-        # Update role group if provided
-        if role:
-            # Remove from all existing groups
-            instance.groups.clear()
-            # Add to new role group
-            group, created = Group.objects.get_or_create(name=role)
-            instance.groups.add(group)
-        
         return instance
